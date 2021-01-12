32919ec08328f238a72b0539ef35175a98332257
maxime-bernard-2 <maxime.bernard.2@etu.univ-amu.fr>
2021-01-12 09:59:27 +0100

    Test fix #9

--- core/Application.php
+++ core/Application.php
@@ -5,7 +5,6 @@ class Application
 {
     public function loadEnv() {
         $handle = @fopen(".env", 'rb');
-        
         if ($handle) {
             while (($buffer = fgets($handle, 4096)) !== false) {
                 if($buffer[0] !== '#' && !ctype_space($buffer)) {
