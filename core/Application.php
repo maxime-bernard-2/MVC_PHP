d7bc1647d1e34695d0f047e297212988c238e1eb
maxime-bernard-2 <maxime.bernard.2@etu.univ-amu.fr>
2021-01-12 11:17:20 +0100

    Correction bracket

--- core/Application.php
+++ core/Application.php
@@ -53,19 +53,21 @@ class Application
         echo $this->router->resolve();
     }
   
-     public function loadEnv() {
-        $handle = @fopen(".env", 'rb');
+     public function loadEnv()
+     {
+         $handle = @fopen(".env", 'rb');
 
-        if ($handle) {
-            while (($buffer = fgets($handle, 4096)) !== false) {
-                if($buffer[0] !== '#' && !ctype_space($buffer)) {
-                    $contant = explode('=', $buffer);
-                    $_ENV[$contant[0]] = $contant[1];
-                }
-            }
-            if (!feof($handle)) {
-                echo "Erreur: veuillez verifier votre .env\n";
-            }
-            fclose($handle);
-        }
+         if ($handle) {
+             while (($buffer = fgets($handle, 4096)) !== false) {
+                 if ($buffer[0] !== '#' && !ctype_space($buffer)) {
+                     $contant = explode('=', $buffer);
+                     $_ENV[$contant[0]] = $contant[1];
+                 }
+             }
+             if (!feof($handle)) {
+                 echo "Erreur: veuillez verifier votre .env\n";
+             }
+             fclose($handle);
+         }
+     }
 }
