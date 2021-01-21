44e9bf1fd29daa0bd56b19437c6b042e4fbec497
unknown <thomas.fillols@etu.univ-amu.fr>
2021-01-21 12:08:51 +0100

    Ajout de la base de la documentation

---
+++ controllers/DocumentationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+use app\models\Documentation;
+
+class DocumentationController extends Controller
+{
+    public function index() {
+
+        $documentation = new Documentation();
+
+        return $this->render('docs/documentation.html.twig', array(
+            'test' => 'Une phrase de test'
+        ));
+    }
+}
